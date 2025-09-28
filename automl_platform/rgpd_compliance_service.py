ba1d594bebcbb7a879d4f61ccb54e0e839a638d4
kv-ops <karthiganvijey@gmail.com>
2025-09-28 03:15:10 +0200

    Update rgpd_compliance_service.py

--- automl_platform/rgpd_compliance_service.py
+++ automl_platform/rgpd_compliance_service.py
@@ -21,7 +21,7 @@ import secrets
 try:
     from cryptography.fernet import Fernet
     from cryptography.hazmat.primitives import hashes
-    from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2
+    from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
     CRYPTO_AVAILABLE = True
 except ImportError:
     CRYPTO_AVAILABLE = False
